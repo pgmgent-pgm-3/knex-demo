3c13410f317aee70c11ca1296ffd266de831a74e
brunowillems <bruno.willems@student.arteveldehs.be>
2025-02-25 09:06:06 +0100

    fork and clone created + installed knex, objection and sqlite3

---
+++ knexfile.js
@@ -0,0 +1,8 @@
+/*
+  This file is used to configure the database connection for the Knex library.
+  Depending on the environment, the configuration will be different.
+  For now, we only have a development environment (also defined in .env file).
+*/
+const config = {};
+
+export default config;
