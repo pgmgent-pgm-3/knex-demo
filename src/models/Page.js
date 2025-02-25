6766b54b244748cce3429ec11be4f58bd5615c05
brunowillems <bruno.willems@student.arteveldehs.be>
2025-02-25 13:52:17 +0100

    addon and filling in of pages model

---
+++ src/models/Page.js
@@ -0,0 +1,32 @@
+import knex from "../lib/Knex.js";
+import { Model } from "objection";
+
+// instantiate the model
+Model.knex(knex);
+
+// define the NavigationItem model
+class pages extends Model {
+    static get tableName() {
+        return "pages";
+    }
+
+    static get idColumn() {
+        return "id";
+    }
+
+    static get jsonSchema() {
+        return {
+            type: "object",
+            required: ["title", "slug", "content"],
+            properties: {
+                id: { type: "integer" },
+                title: { type: "string", minLength: 1, maxLength: 255 },
+                slug: { type: "string", maxLength: 255 },
+                content: { type: "string", minLength: 1, maxLength: 255 },
+                is_homepage: { type: "boolean" },
+            },
+        };
+    }
+}
+
+export default NavigationItem;
